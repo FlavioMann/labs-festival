2307f64a9b77def2819cffab9307025196076a1b
Flavio Mann <flavioamann@gmail.com>
2026-02-27 10:39:22 -0300

    build: update import HeroVideo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,12 +8,12 @@ import ExclusiveContent from "@/components/sections/ExclusiveContent";
 import FeaturedBanner from "@/components/sections/FeaturedBanner";
 import FeaturedBannerDigital from "@/components/sections/FeaturedBannerDigital";
 import GenreMusicalFestival from "@/components/sections/GenreMusicalFestival";
-import HeroVideo from "@/components/sections/HeroVideo";
 import InLive from "@/components/sections/InLive";
 import LineUp from "@/components/sections/LineUp";
 import RockSingers from "@/components/sections/RockSingers";
 import WatchAgain from "@/components/sections/WatchAgain";
 import YesterdayShows from "@/components/sections/YesterdayShows";
+import HeroVideo from "@/components/sections/HeroVideo";
 
 export default function Home() {
   return (
